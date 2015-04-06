05e4e390f27cbc84ecb7736384c7186767b79fe7
pho <pho@damnedsouls.de>
2015-04-06 20:19:53 +0200

    Zip is doch echt dämlich

--- rollout.sh
+++ rollout.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+CURENT_PATH=$(pwd)
 rm -rf ./../PillarsOfEternity_Data
 rm ./../latest.zip
 git checkout-index -a --prefix=../PillarsOfEternity_Data/data/localized/de-patched/
-zip ../latest.zip -r -q ../PillarsOfEternity_Data/ -x@../PillarsOfEternity_Data/data/localized/de-patched/exclude.lst
+cd ..
+zip latest.zip -r -q PillarsOfEternity_Data/ -x@PillarsOfEternity_Data/data/localized/de-patched/exclude.lst
+cd $CURRENT_PATH

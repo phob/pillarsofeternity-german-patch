1c867aa2108bf958536867621276c643f6a547c0
pho <pho@damnedsouls.de>
2015-04-07 22:31:22 +0200

    modified:   exclude.lst

--- rollout.sh
+++ rollout.sh
@@ -4,5 +4,5 @@ rm -rf ./../PillarsOfEternity_Data
 rm ./../latest.zip
 git checkout-index -a --prefix=../PillarsOfEternity_Data/data/localized/de-patched/
 cd ..
-zip latest.zip -r -q PillarsOfEternity_Data/ -x@PillarsOfEternity_Data/data/localized/de-patched/exclude.lst
+zip latest.zip -9 -r -q PillarsOfEternity_Data/ -x@PillarsOfEternity_Data/data/localized/de-patched/exclude.lst
 cd $CURRENT_PATH
